2e28feaf3230e2697fcd7303575c53ffbb6f5dbd
NureKhudiienkoMykyta <mykyta.khudiienko@nure.ua>
2026-03-28 15:37:49 +0200

    Change task2

--- Pract1/task2.py
+++ Pract1/task2.py
@@ -1,12 +1,7 @@
 #1.Напишіть програму, яка знаходить середнє значення з трьох чисел, введених користувачем.
-num1 = input("Введіть перше число: ")
-num2 = input("Введіть друге число: ")
-num3 = input("Введіть третє число: ")
+num1 = float(input("Введіть перше число: "))
+num2 = float(input("Введіть друге число: "))
+num3 = float(input("Введіть третє число: "))
 
-if num1.isdigit() and num2.isdigit() and num3.isdigit():
-    avg = (int(num1) + int(num2) + int(num3)) / 3
-    print(f"Середнє значення: {avg:.5f}")
-else:
-    print("Ви ввели не число!")
-
-
+avg = (num1 + num2 + num3) / 3
+print(f"Середнє значення: {avg}")
